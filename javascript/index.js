5f51c6b2f778d22286c87e007b53b1b15b584c9a
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-05-30 17:33:55 -0400

    added real data for date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector("#date");
+  let parisTimeElement = parisElement.querySelector("#time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("dddd Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector("#date");
+  let londonTimeElement = londonElement.querySelector("#time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("dddd Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
